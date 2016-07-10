53469efa1ce306230fc59ded5876c5490195cd0d
Marcin Kossowski <marcin.kossowski@24hr.se>
2016-07-10 18:42:57 +0200

    Fixes for mobile

--- games/color-zap/js/stateTitle.js
+++ games/color-zap/js/stateTitle.js
@@ -12,7 +12,8 @@ var stateTitle = {
    create: function () {
       'use strict';
       
-      this.logo = game.add.sprite(0, 0, "logo");
+      this.logo = game.add.sprite(game.world.centerX, 180, "logo");
+      this.logo.anchor.set(0.5, 0.5);
    },
    
    //Constant running loop.
